caaad08e4db18252511a8c86369a782d992739ec
Jose Maria Souza de Oliveira <jmcriat@gmail.com>
2015-09-30 00:03:05 -0300

    test

--- code.js
+++ code.js
@@ -1,15 +1,14 @@
 void setup() {
-  size(400, 400);
+  size(screen.width*3/4, screen.height*3/4);
 }
 
 var x = 100;
-var y = 300;
-
+var y = screen.height*2/3*3/4;
 
 void draw() {
     background(206, 144, 209);
     fill(130, 173, 157);
-    rect(0,300,400,100);
+    rect(0,height*2/3,width,height);
     fill(245, 245, 61);
     // static
     if(keyPressed) {
